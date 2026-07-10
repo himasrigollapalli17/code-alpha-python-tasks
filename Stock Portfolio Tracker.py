c0151963c7a5c3ee66ad62e82d520d82a003614e
hima <himasrigollapalli17@gmail.com>
2026-07-10 12:19:36 +0530

    updated Stock Portfolio Tracker

--- Stock Portfolio Tracker.py
+++ Stock Portfolio Tracker.py
@@ -1,37 +1,33 @@
-# Stock Portfolio Tracker
-
-# Hardcoded stock prices
-stock_prices = {
-    "AAPL": 180,
-    "TSLA": 250,
-    "GOOG": 150,
-    "AMZN": 200
+stocks = {
+    "A": 3500,
+    "B": 1600,
+    "C": 2900,
+    "D": 1700,
+    "E": 450
 }
 
-total_investment = 0
+stock_name = input("Enter stock name: ").upper()
 
-# Number of stocks user wants to enter
-n = int(input("Enter number of stocks: "))
-
-for i in range(n):
-    stock_name = input("Enter stock name: ").upper()
+if stock_name in stocks:
     quantity = int(input("Enter quantity: "))
 
-    # Check if stock exists
-    if stock_name in stock_prices:
-        investment = stock_prices[stock_name] * quantity
-        total_investment += investment
+    total = stocks[stock_name] * quantity
 
-        print("Investment for", stock_name, "=", investment)
-    else:
-        print("Stock not found!")
+    print("\n----- Stock Details -----")
+    print("Stock Name :", stock_name)
+    print("Price      :", stocks[stock_name])
+    print("Quantity   :", quantity)
+    print("Total Cost :", total)
 
-# Display total investment
-print("\nTotal Investment Value =", total_investment)
+    choice = input("\nDo you want to save the details? (yes/no): ").lower()
 
-# Save result into a text file
-file = open("portfolio.txt", "w")
-file.write("Total Investment Value = " + str(total_investment))
-file.close()
-
-print("Result saved in portfolio.txt")
+    if choice == "yes":
+        file = open("stock_details.txt", "w")
+        file.write("Stock Name: " + stock_name + "\n")
+        file.write("Price: " + str(stocks[stock_name]) + "\n")
+        file.write("Quantity: " + str(quantity) + "\n")
+        file.write("Total Cost: " + str(total))
+        file.close()
+        print("Details saved successfully!")
+else:
+    print("Stock not available.")
